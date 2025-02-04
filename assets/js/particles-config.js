25f584a3211585efe04936d1c0627c819bab2e8c
icparanoa <pedrohcorreia083@gmail.com>
2025-02-04 09:17:29 -0300

    .

---
+++ assets/js/particles-config.js
@@ -0,0 +1,47 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 120,
+        "density": { "enable": true, "value_area": 600 }
+      },
+      "color": { "value": "#00625F" },
+      "shape": { "type": "polygon" },
+      "opacity": {
+        "value": 0.7,
+        "random": true,
+        "anim": { "enable": false }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": { "enable": false }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#00aaff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out"
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": { "enable": true, "mode": "repulse" },
+        "onclick": { "enable": true, "mode": "push" }
+      },
+      "modes": {
+        "repulse": { "distance": 100, "duration": 0.4 },
+        "push": { "particles_nb": 4 }
+      }
+    }
+  });
+  
